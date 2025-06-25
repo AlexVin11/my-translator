07a1f9780ccfae19979833ef39e08d24c1b1f865
Alexander Vinogradow <alukard981@gmail.com>
2025-06-25 15:37:35 +0300

    Initial project commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.vineberger"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["sentryVersion"] = "8.14.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.sentry:sentry-spring-boot-starter-jakarta")
+	implementation("org.springframework.kafka:spring-kafka")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation("org.assertj:assertj-core:3.26.3")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("net.datafaker:datafaker:2.0.2")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("io.sentry:sentry-bom:${property("sentryVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/vineberger/avinecode/MyCustomTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.vineberger.avinecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCustomTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCustomTranslatorApplication.class, args);
+	}
+
+}
