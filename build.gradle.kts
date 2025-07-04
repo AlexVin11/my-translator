af86c551429638e08473666c0b15d0a09c7801c2
Alexander Vinogradow <alukard981@gmail.com>
2025-07-04 15:16:27 +0300

    caching dependency fixing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
+	id("com.github.ben-manes.versions") version "0.49.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -28,6 +29,7 @@ extra["sentryVersion"] = "8.14.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
