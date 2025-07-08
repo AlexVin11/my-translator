2091cfe6381330c81dc79f3eb20da39f115a81cf
Alexander Vinogradow <alukard981@gmail.com>
2025-07-08 16:56:13 +0300

    mvc

--- src/main/java/com/vineberger/avinecode/controller/languageslist/AvailableLanguageController.java
+++ src/main/java/com/vineberger/avinecode/controller/languageslist/AvailableLanguageController.java
@@ -1,23 +1,35 @@
 package com.vineberger.avinecode.controller.languageslist;
 
-import com.vineberger.avinecode.dto.yandexairesponsemessage.AvailableLanguage;
+import com.vineberger.avinecode.config.YandexConfig;
+import com.vineberger.avinecode.dto.yandexairesponsemessage.Language;
 import com.vineberger.avinecode.service.TranslationLanguagesListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.ServiceUnavailableException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/languages")
 @RequiredArgsConstructor
 public class AvailableLanguageController {
-    private final TranslationLanguagesListService languagesListService;
+    private final YandexConfig yandexConfig; //тут урл внешнего сервиса
+    private final TranslationLanguagesListService LANGUAGE_LIST_SERVICE;
 
-    @GetMapping
-    public ResponseEntity<List<AvailableLanguage>> getLanguages() {
-        return ResponseEntity.ok(languagesListService.getAllLanguages());
+    @PostMapping
+    public ResponseEntity<?> getLanguages() {
+        try {
+            List<Language> languages = LANGUAGE_LIST_SERVICE.getAvailableLanguages();
+            return ResponseEntity.ok(Map.of("languages", languages));
+        } catch (ServiceUnavailableException e) {
+            return ResponseEntity
+                    .status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(e.getMessage());
+        }
     }
 }
