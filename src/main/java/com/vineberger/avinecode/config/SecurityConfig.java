f152374cb8b86f7f054aceb38f2454531030f997
Alexander Vinogradow <alukard981@gmail.com>
2025-07-08 16:53:26 +0300

    mvc

--- src/main/java/com/vineberger/avinecode/config/SecurityConfig.java
+++ src/main/java/com/vineberger/avinecode/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vineberger.avinecode.config;
 
+import com.vineberger.avinecode.handler.JwtAuthenticationSuccessHandler;
 import com.vineberger.avinecode.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,8 +19,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.UUID;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -30,18 +33,19 @@ public class SecurityConfig {
     private final JwtDecoder jwtDecoder;
     private final PasswordEncoder passwordEncoder;
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationSuccessHandler jwtSuccessHandler;
 
     @Bean
     @Order(1)
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
+                .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
-                        .requestMatchers("/languages").permitAll()
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/api/translate").permitAll()//не смог разобраться с аутентификацией на фронте поэтому сделал так
                         .anyRequest().authenticated())
-                .securityMatcher("/api/**")
                 .oauth2ResourceServer(rs -> rs.jwt(jwt -> jwt.decoder(jwtDecoder)))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -52,16 +56,45 @@ public class SecurityConfig {
     @Order(2)
     public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**", "/languages", "/translate")
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(headers -> headers.frameOptions().sameOrigin())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/languages").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/webjars/**", "/resources/**", "/static/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/favicon.ico", "/error").permitAll()
+                        .requestMatchers("/webjars/**", "/static/**", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/languages").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/translate").permitAll()
+                        .requestMatchers("/maintranslationforms").authenticated()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll());
+                .formLogin(form -> form
+                                .defaultSuccessUrl("/maintranslationforms", true)
+                                .successHandler(jwtSuccessHandler)
+                        // Убрали .loginPage("/login") чтобы использовать стандартную форму
+                        /*.defaultSuccessUrl("/maintranslationforms", true)
+                        .permitAll()*/)
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
