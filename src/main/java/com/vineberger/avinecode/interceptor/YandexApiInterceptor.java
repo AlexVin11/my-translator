3e0df9537205af2771fbc7d92de77fe20e7eff70
Alexander Vinogradow <alukard981@gmail.com>
2025-07-08 16:59:25 +0300

    mvp

--- src/main/java/com/vineberger/avinecode/interceptor/YandexApiInterceptor.java
+++ src/main/java/com/vineberger/avinecode/interceptor/YandexApiInterceptor.java
@@ -3,23 +3,24 @@ package com.vineberger.avinecode.interceptor;
 import com.vineberger.avinecode.config.YandexConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class YandexApiInterceptor implements ClientHttpRequestInterceptor {
 
-    private final YandexConfig yandexConfig;
+    private final YandexConfig YANDEX_CONFIG;
 
-    public ClientHttpResponse intercept(HttpRequest request,
-                                        byte[] body,
-                                        ClientHttpRequestExecution execution) throws IOException {
-
-        request.getHeaders().set("Authorization", "Api-Key " + yandexConfig.getApiKey());
-
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        request.getHeaders().set("Authorization", "Api-Key " + YANDEX_CONFIG.getApiKey());
         return execution.execute(request, body);
     }
 }
