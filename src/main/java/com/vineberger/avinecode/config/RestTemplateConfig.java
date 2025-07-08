35c8f453dd6c140c9dc223ad502c650f3513669b
Alexander Vinogradow <alukard981@gmail.com>
2025-07-08 16:57:38 +0300

    mvp

--- src/main/java/com/vineberger/avinecode/config/RestTemplateConfig.java
+++ src/main/java/com/vineberger/avinecode/config/RestTemplateConfig.java
@@ -1,15 +1,23 @@
 package com.vineberger.avinecode.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import com.vineberger.avinecode.interceptor.YandexApiInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateConfig {
 
+    private final YandexApiInterceptor INTERCEPTOR;
+
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(List.of(INTERCEPTOR));
+        return restTemplate;
     }
 }
